72f81c1013ad38b87a519fd943c737fa63e4c504
zeke <zeke@sikelianos.com>
2012-01-30 11:41:42 -0800

    Added a link to a screenshot

--- twitter.com.js
+++ twitter.com.js
@@ -1,4 +1,5 @@
 // Check for tweets that start with '@' and quiet 'em down a bit.
+// Here's what it looks like: http://cl.ly/Dl86
 setInterval(
   function() {
     $('div.js-stream-item').each(function() {
