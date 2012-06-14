73e68ac4970da84c44c4e6dc3033f69e58cd91cb
zeke <zeke@sikelianos.com>
2012-06-13 22:36:17 -0700

    cheat.errtheblog.com

---
+++ cheat.errtheblog.com.js
@@ -0,0 +1,22 @@
+// http://cl.ly/HOgt
+
+$('body').css({
+  background: '#CCC',
+  color: '#333'
+  
+  // Also nice...
+  // background: '#333',
+  // color: '#CCC'
+});
+
+$('.header, .side, .footer').css({
+  display: 'none'
+});
+
+$('.version').css({
+  background: 'none',
+  color: '#666',
+  fontFamily: 'monospace',
+  whiteSpace: 'pre',
+  margin: '1em 0px'
+});
